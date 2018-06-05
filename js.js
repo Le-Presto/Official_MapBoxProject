6f8e6461616610a5680cdfa1cab7c0cf89bbfb7c
aiden <aidenle@students.berkeley.net>
2018-06-05 11:24:49 -0700

    dunv1

--- js.js
+++ js.js
@@ -1,32 +1,4 @@
-$.ajax({
-    type: 'POST',
-    contentType: 'application/json',
-    data: {
-        "email": "myEmail@students.berkeley.net",
-        "password": "isilly"
-    },
-    dataType: 'json',
-    success: function(data){
-        runMySuccessFunction(data);
-    },
-    error: function(){
-        alert("failed");
-    },
-    url: 'https://slkidsbackend.herokuapp.com/[appName]/api/users'
-});
 
-//get a user out of the db
-$.ajax({
-    type: 'GET',
-    dataType: 'json',
-    success: function(data){
-        runMySuccessFunction(data);
-    },
-    error: function(){
-        alert("failed");
-    },
-    url: 'https://slkidsbackend.herokuapp.com/[appName]/api/users/' + emailToGet
-});
 
 function showMap() {
     $("#welcome").hide();
